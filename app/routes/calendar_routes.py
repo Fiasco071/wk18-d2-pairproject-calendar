663ac12a9c0355a5b0571b81be503d9fe53904eb
Daniel Blanco <db@d-blanco.com>
2022-04-19 18:47:08 -0400

    database & first table DONE

--- app/routes/calendar_routes.py
+++ app/routes/calendar_routes.py
@@ -6,22 +6,9 @@ bp = Blueprint("main", __name__, url_prefix="/")
 CONNECTION_PARAMETERS = {
     "dbname": "calendar_this_dev",
     "user": "calendar_this",
-    "password": "password"
+    "password": "Ku9rSyXD"
 }
 
 @bp.route("/")
 def main():
-    # return render_template("main.html", title='Flask App Project') 
-
-    with psycopg2.connect(**CONNECTION_PARAMETERS) as banana:
-        with banana.cursor() as curs:
-            def create_table():
-                curs.execute(
-                    """
-                    CREATE TABLE calendar (
-                    id SERIAL PRIMARY KEY,
-                    day VARCHAR(50),
-                    fieldone VARCHAR(50)
-                    )
-                    """
-                )
+    # return render_template("main.html", title='Flask App Project')
